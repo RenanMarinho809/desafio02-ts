5169bbafb6758a0c1646e271af8e108eb2018898
Renanmdev <renanmarinho809@gmail.com>
2024-10-13 19:51:48 -0300

    Implementando header no projeto Diobank

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
-import { 
-  Center,
-  ChakraProvider,
-  Input,
-  Box,
-  Button
-} from '@chakra-ui/react'
-import { login } from './services/login';
+import { Box } from "@chakra-ui/react";
+import Header from "./components/Header/Header";
 
-function App() {
+
+
+const HomePage: React.FC = () => {
   return (
-    <ChakraProvider>
-      <Box minHeight='100vh' backgroundColor='#9413dc' padding='25px'>
-        <Box backgroundColor='#FFFFFF' borderRadius='25px' padding='15px' >
-          <Center>
-            <h1>Faça o login</h1>
-          </Center>
-          <Input placeholder="email" />
-          <Input placeholder="password" />
-          <Center>
-            <Button onClick={login} colorScheme='teal' size='sm' width='100%' marginTop='5px'>
-              Button
-            </Button>
-          </Center>
-        </Box>
-      </Box>
-    </ChakraProvider>
+    <Box textAlign="center" p={4}>
+         <Header />
+    </Box>
   );
-}
+};
 
-export default App;
+export default HomePage;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,13 @@
-import './Header.css'
+import { Box, Heading } from "@chakra-ui/react";
 
-export const Header  = () => {
-  return(
-    <div className='header'>
-      Dio Bank
-    </div>
-  )
-}
+const Header: React.FC = () => {
+  return (
+    <Box as="header" bg="teal.500" py={4} textAlign="center" >
+      <Heading as="h1" color="white">
+        Minha Aplicação
+      </Heading>
+    </Box>
+  );
+};
+
+export default Header;
